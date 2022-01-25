0eb9014f5f7f3708fbf6c1d6c925cfeb565a2eb0
xhloo <97490258+xhloo@users.noreply.github.com>
2022-01-25 10:54:30 +0800

    wk01 tutorial

---
+++ Q2c.java
@@ -0,0 +1,14 @@
+public class Q2c 
+{
+	public static void main(String arg[])   
+    {   
+        for(int x = 102; x > 66; x = x-1)
+        {
+        	if(x % 2 == 1)
+        	{
+        		System.out.print("value of x: "+ x);
+        	System.out.print("\n");
+        	}
+        }
+    }    
+}
